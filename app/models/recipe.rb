850e79e5783d7ff149992cae9cee3954249d9380
mghadley <matthew.g.hadley@gmail.com>
2016-01-24 19:21:40 -0700

    Added validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ActiveRecord::Base
+	validates :title, presence: true, uniqueness: { case_sensitive: false }
 	has_and_belongs_to_many :ingredients
 	accepts_nested_attributes_for :ingredients
 	belongs_to :category

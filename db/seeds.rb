05f6b2f3258f2b566f7180e6122102e74c28dcb4
mghadley <matthew.g.hadley@gmail.com>
2016-01-24 20:39:08 -0700

    updating categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Category.create([{title: "Entree"}, {title: "Dessert"}, {title: "Bread"}])
+Category.create([{title: "Appetizers"}, {title: "Soups"}, {title: "Main Dishes"}, {title: "Breakfast"}, {title: "Beverages"},
+								 {title: "Desserts"}, {title: "Breads"}, {title: "Side Dishes"}, {title: "Snacks"}, {title: "Misc."}, 
+								 {title: "Canning"}])
